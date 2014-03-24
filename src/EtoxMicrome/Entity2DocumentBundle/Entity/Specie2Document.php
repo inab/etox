8f476ba08bf43537be3f6cb9761293c4498453ba
acanada <acanada@cnio.es>
2014-03-24 16:01:45 +0100

    species2document and javascript changes

---
+++ src/EtoxMicrome/Entity2DocumentBundle/Entity/Specie2Document.php
@@ -0,0 +1,399 @@
+<?php
+
+namespace EtoxMicrome\Entity2DocumentBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Specie2Document
+ *
+ * @ORM\Table(indexes={@ORM\Index(name="specie2Document_document_index", columns={"document_id"}), @ORM\Index(name="specie2Document_specie_index", columns={"specie_id"}), @ORM\Index(name="specie2document_patterncount", columns={"""patternCount"""}), @ORM\Index(name="specie2document_rulescore", columns={"""ruleScore"""}), @ORM\Index(name="specie2document_heptermnormscore", columns={"""hepTermNormScore"""}), @ORM\Index(name="specie2document_heptermvarscore", columns={"""hepTermVarScore"""}), @ORM\Index(name="specie2document_svmconfidence", columns={"""svmConfidence"""}) } )
+ * @ORM\Entity(repositoryClass="EtoxMicrome\Entity2DocumentBundle\Entity\Specie2DocumentRepository")
+ */
+class Specie2Document
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="EtoxMicrome\DocumentBundle\Entity\Document", inversedBy="specie2document")
+     * @ORM\JoinColumn(name="document_id", referencedColumnName="id")
+     */
+
+    private $document;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="EtoxMicrome\EntityBundle\Entity\Specie", inversedBy="specie2document")
+     * @ORM\JoinColumn(name="specie_id", referencedColumnName="id")
+     */
+    private $specie;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="hepval", type="float", nullable=true)
+     */
+    private $hepval;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="cardval", type="float", nullable=true)
+     */
+    private $cardval;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="nephval", type="float", nullable=true)
+     */
+    private $nephval;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="phosval", type="float", nullable=true)
+     */
+    private $phosval;
+
+     /**
+     * @var float
+     *
+     * @ORM\Column(name="""patternCount""", type="float", nullable=true)
+     */
+    private $patternCount;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="""ruleScore""", type="float", nullable=true)
+     */
+    private $ruleScore;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="""hepTermNormScore""", type="float", nullable=true)
+     */
+    private $hepTermNormScore;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="""hepTermVarScore""", type="float", nullable=true)
+     */
+    private $hepTermVarScore;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="""svmConfidence""", type="float", nullable=true)
+     */
+    private $svmConfidence;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created", type="datetime")
+     */
+    private $created;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="updated", type="datetime", nullable=true)
+     */
+    private $updated;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set document
+     */
+    public function setDocument(\EtoxMicrome\DocumentBundle\Entity\Document $document)
+    {
+        $this->document = $document;
+        return $this;
+    }
+
+    /**
+     * Get document
+     */
+    public function getDocument()
+    {
+        return $this->document;
+    }
+
+    /**
+     * Set specie
+     */
+    public function setSpecie(\EtoxMicrome\EntityBundle\Entity\Specie $specie)
+    {
+        $this->specie = $specie;
+        return $this;
+    }
+
+    /**
+     * Get specie
+     */
+    public function getSpecie()
+    {
+        return $this->specie;
+    }
+
+    /**
+     * Set hepval
+     *
+     * @param float $hepval
+     * @return Specie2Document
+     */
+    public function setHepval($hepval)
+    {
+        $this->hepval = $hepval;
+
+        return $this;
+    }
+
+    /**
+     * Get hepval
+     *
+     * @return float
+     */
+    public function getHepval()
+    {
+        return $this->hepval;
+    }
+
+    /**
+     * Set cardval
+     *
+     * @param float $cardval
+     * @return Specie2Document
+     */
+    public function setCardval($cardval)
+    {
+        $this->cardval = $cardval;
+
+        return $this;
+    }
+
+    /**
+     * Get cardval
+     *
+     * @return float
+     */
+    public function getCardval()
+    {
+        return $this->cardval;
+    }
+
+    /**
+     * Set nephval
+     *
+     * @param float $nephval
+     * @return Specie2Document
+     */
+    public function setNephval($nephval)
+    {
+        $this->nephval = $nephval;
+
+        return $this;
+    }
+
+    /**
+     * Get nephval
+     *
+     * @return float
+     */
+    public function getNephval()
+    {
+        return $this->nephval;
+    }
+
+    /**
+     * Set phosval
+     *
+     * @param float $phosval
+     * @return Specie2Document
+     */
+    public function setPhosval($phosval)
+    {
+        $this->phosval = $phosval;
+
+        return $this;
+    }
+
+    /**
+     * Get phosval
+     *
+     * @return float
+     */
+    public function getPhosval()
+    {
+        return $this->phosval;
+    }
+
+    /**
+     * Get patternCount
+     *
+     * @return float
+     */
+    public function getPatternCount()
+    {
+        return $this->patternCount;
+    }
+
+    /**
+     * Set ruleScore
+     *
+     * @param float $ruleScore
+     * @return Specie2Document
+     */
+    public function setRuleScore($ruleScore)
+    {
+        $this->ruleScore = $ruleScore;
+
+        return $this;
+    }
+
+    /**
+     * Get ruleScore
+     *
+     * @return float
+     */
+    public function getRuleScore()
+    {
+        return $this->ruleScore;
+    }
+
+    /**
+     * Set hepTermNormScore
+     *
+     * @param float $hepTermNormScore
+     * @return Specie2Document
+     */
+    public function setHepTermNormScore($hepTermNormScore)
+    {
+        $this->hepTermNormScore = $hepTermNormScore;
+
+        return $this;
+    }
+
+    /**
+     * Get hepTermNormScore
+     *
+     * @return float
+     */
+    public function getHepTermNormScore()
+    {
+        return $this->hepTermNormScore;
+    }
+
+    /**
+     * Set hepTermVarScore
+     *
+     * @param float $hepTermVarScore
+     * @return Specie2Document
+     */
+    public function setHepTermVarScore($hepTermVarScore)
+    {
+        $this->hepTermVarScore = $hepTermVarScore;
+
+        return $this;
+    }
+
+    /**
+     * Get hepTermVarScore
+     *
+     * @return float
+     */
+    public function getHepTermVarScore()
+    {
+        return $this->hepTermVarScore;
+    }
+
+    /**
+     * Set svmConfidence
+     *
+     * @param float $svmConfidence
+     * @return Specie2Document
+     */
+    public function setSvmConfidence($svmConfidence)
+    {
+        $this->svmConfidence = $svmConfidence;
+
+        return $this;
+    }
+
+    /**
+     * Get svmConfidence
+     *
+     * @return float
+     */
+    public function getSvmConfidence()
+    {
+        return $this->svmConfidence;
+    }
+
+
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     * @return Specie2Document
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    /**
+     * Get created
+     *
+     * @return \DateTime
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * Set updated
+     *
+     * @param \DateTime $updated
+     * @return Specie2Document
+     */
+    public function setUpdated($updated)
+    {
+        $this->updated = $updated;
+
+        return $this;
+    }
+
+    /**
+     * Get updated
+     *
+     * @return \DateTime
+     */
+    public function getUpdated()
+    {
+        return $this->updated;
+    }
+}
