52208a324328b3cc336e69f300c7b0d16ced1d8c
acanada <acanada@cnio.es>
2014-11-24 15:53:42 +0100

    compoundName2Cas page

--- src/EtoxMicrome/FrontendBundle/Controller/DefaultController.php
+++ src/EtoxMicrome/FrontendBundle/Controller/DefaultController.php
@@ -23,4 +23,31 @@ class DefaultController extends Controller
         $respuesta = $this->render('FrontendBundle:Default:tutorial.html.twig');
         return $respuesta;
     }
+
+    public function compoundName2CasAction()
+    {
+        $arrayCompoundsCas=array();
+        //We read the file with the pairs  compoundName-CASregistry_number and save it into an arrayt
+        $request = $this->get('request');
+        $path = $this->get('kernel')->getRootDir(). "/../web/files/";
+        $filepath=$path."compoundName2CAS.tsv";
+        //ld($filepath);
+        $arrayContent = file($filepath);
+        foreach($arrayContent as $line)
+        {
+            #ld($line);
+            //get row data
+            $arrayLine = explode("\t", $line);
+            $compoundName=$arrayLine[0];
+            $cas=$arrayLine[1];
+            $cas=trim($cas);
+            $arrayCompoundsCas[$cas]=$compoundName;
+        }
+
+
+        $respuesta = $this->render('FrontendBundle:Default:compoundName2Cas.html.twig', array(
+            'arrayCompoundsCas'      => $arrayCompoundsCas,
+        ));
+        return $respuesta;
+    }
 }
